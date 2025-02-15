993e0f0a4806c974aa96bcf99c571e369aeb4e6f
Sundara Moorthy S <30520393+ssmoorthys@users.noreply.github.com>
2025-02-15 21:36:44 +0530

    Update Config.java

--- ex3/src/main/java/com/mystore/app/Config.java
+++ ex3/src/main/java/com/mystore/app/Config.java
@@ -1,1 +1,18 @@
+package com.mystore.app;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.mystore.app")
+public class Config {
+
+    @Bean
+    public Product tumblerProduct() {
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Tumbler");
+        return product;
+    }
+}
